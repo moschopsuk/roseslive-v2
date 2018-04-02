2025d314874710130c079b4cea229a8fd3351485
Luke Moscrop <luke.moscrop@bbc.co.uk>
2018-04-02 15:48:24 +0100

    convert ctx making it easier to dev

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,9 +1,10 @@
+import { DisciplineService } from '../services/disciplineService';
 import NotFoundError from './errors/notfoundError';
 
 export const resolvers = {
     Query: {
         async discipline(obj, { id }, ctx, info) {
-            const service = ctx.disciplineService;
+            const service = (ctx.disciplineService as DisciplineService);
             const discipline = await service.find(id);
             if (!discipline) {
                 throw new NotFoundError('discipline',  id);
@@ -12,7 +13,7 @@ export const resolvers = {
         },
 
         async disciplines(obj, args, ctx, info) {
-            const service = ctx.disciplineService;
+            const service = (ctx.disciplineService as DisciplineService);
             return await service.getAll();
         },
     },
